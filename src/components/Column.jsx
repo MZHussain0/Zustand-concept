5a01fb79c6ce991fcb7924c3ebbeae79d33e6e42
mzhussain0 <mzhussain092@gmail.com>
2023-09-17 12:06:41 +0530

    zustand

---
+++ src/components/Column.jsx
@@ -0,0 +1,47 @@
+﻿import { useStores } from "../../store";
+import { shallow } from "zustand/shallow";
+import "./Column.css";
+import Task from "./Task";
+import { useState } from "react";
+export default function Column({ state }) {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+  const tasks = useStores(
+    (store) => store.tasks.filter((task) => task.state === state),
+    shallow
+  );
+
+  const addTask = useStores((store) => store.addTask);
+  return (
+    <div className="column">
+      <div className="titleWrapper">
+        <p>{state}</p>
+        <button onClick={() => setOpen(true)}>Add</button>
+      </div>
+      {tasks.map((task) => (
+        <Task title={task.title} key={task.title} />
+      ))}
+
+      {open && (
+        <div className="modal">
+          <div className="modalContent">
+            <input
+              type="text"
+              placeholder="Title..."
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <button
+              onClick={() => {
+                addTask(text, state);
+                setText("");
+                setOpen(false);
+              }}>
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/components/Task.jsx
@@ -0,0 +1,19 @@
+﻿import { useStores } from "../../store";
+import "./task.css";
+import classNames from "classnames";
+
+export default function Task({ title }) {
+  const task = useStores((store) =>
+    store.tasks.find((task) => task.title === title)
+  );
+
+  return (
+    <div className="task">
+      <div className="">{task.title}</div>
+      <div className="bottomWrapper">
+        <div className=""></div>
+        <div className={classNames("status", task.state)}>{task.state}</div>
+      </div>
+    </div>
+  );
+}
